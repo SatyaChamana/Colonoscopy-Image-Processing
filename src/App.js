aab1c25c9e9aaf4ec5116817be5dcc0c52e6a520
Satya Chamana <satyachamana@gmail.com>
2024-03-09 00:42:06 -0500

    Fixed mp4 video issue

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
-import './App.css';
-import { useEffect, useRef, useState } from 'react';
 
 function App() {
-  const [videoPath, setVideoPath]  = useState("")
+  const [videoUrl, setVideoUrl] = useState('');
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     if (file) {
       const url = URL.createObjectURL(file);
-      setVideoPath(url);
+      setVideoUrl(url);
     }
   };
 
   return (
-    <div className="App">
-      <ReactPlayer url={videoPath} controls width={500} height={500}/>
+    <div>
+      <input type="file" accept="video/*" onChange={handleFileChange} />
+      {videoUrl && <ReactPlayer url={videoUrl} controls />}
     </div>
   );
 }
